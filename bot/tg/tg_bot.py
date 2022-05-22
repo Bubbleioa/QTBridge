b03b09b3482a47adf7561725027fdc45fa25cad5
Bubbleioa <1342130847@qq.com>
2022-05-22 15:39:03 +0800

    fix: decode base64

--- bot/tg/tg_bot.py
+++ bot/tg/tg_bot.py
@@ -67,6 +67,7 @@ def create_telegram_bridge(token,chat_id,blacklist=None,http_proxy=None,loop=Non
                             rep = await session.get(f'https://api.telegram.org/file/bot{token}/{rep["result"]["file_path"]}',proxy=http_proxy)
                             img = await rep.content.read()
                             b64_str = 'data:image/png;base64,'+base64.b64encode(img).decode('UTF-8')
+                            logger.debug("img base64: %s",b64_str)
                             final_msg += f'[CQ:image,file={b64_str}]'
                             if 'caption' in msg:
                                 final_msg += msg['caption']

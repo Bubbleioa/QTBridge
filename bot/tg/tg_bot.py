629782ab407e2a70c0ce97a03a199af1fcd264b6
Bubbleioa <1342130847@qq.com>
2022-05-22 15:11:41 +0800

    fix: telegram picture

--- bot/tg/tg_bot.py
+++ bot/tg/tg_bot.py
@@ -65,7 +65,8 @@ def create_telegram_bridge(token,chat_id,blacklist=None,http_proxy=None,loop=Non
                             rep = await rep.read()
                             rep = json.loads(rep.decode())
                             rep = await session.get(f'https://api.telegram.org/file/bot{token}/{rep["result"]["file_path"]}',proxy=http_proxy)
-                            b64_str = base64.b64encode(rep.content.read())
+                            img = await rep.content.read()
+                            b64_str = base64.b64encode(img)
                             final_msg += f'[CQ:image,file={b64_str}]'
                             if 'caption' in msg:
                                 final_msg += msg['caption']
